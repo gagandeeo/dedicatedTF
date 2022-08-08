662e9c72ed930cb97142f4771fad37e4a33491ed
gagandeeo <gagandeepprasad9@gmail.com>
2022-08-09 00:34:03 +0530

    server-initiated

---
+++ server/download_model.py
@@ -0,0 +1,4 @@
+from tensorflow.keras import applications
+
+model = applications.mobilenet.MobileNet()
+model.save('./mobilenet-model.h5')
---
+++ server/test_model.py
@@ -0,0 +1,26 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras import applications
+from PIL import Image
+import numpy as np
+import io
+
+
+def prepare_image(image_data):
+    img_ = Image.open(io.BytesIO(image_data))
+    img_ = img_.resize((224,224))
+    img_array = image.img_to_array(img_)
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return applications.mobilenet.preprocess_input(img_array_expanded_dims)
+
+def predict(img, model):
+    predictions = np.array(model.predict(img))
+    results = np.array(applications.imagenet_utils.decode_predictions(predictions))
+    res_3 = np.argpartition(predictions[0], -3)[-3:]
+    return str(results)
+
+    
+    
+    
+
+
