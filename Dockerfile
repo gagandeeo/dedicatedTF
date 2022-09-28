844036ed28f25781717e46d9fc6f54b3828d239a
gagandeeo <gagandeepprasad50@gmail.com>
2022-09-28 23:43:27 +0530

    Docker added

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Stage 1: Builder/Compiler
+FROM python:3.7-slim as builder
+RUN apt update && \
+    apt install --no-install-recommends -y build-essential gcc
+COPY requirements.txt /requirements.txt
+
+RUN pip install --no-cache-dir --user -r /requirements.txt
+
+# Stage 2: Runtime
+FROM debian:buster-slim
+RUN apt update && \
+    apt install --no-install-recommends -y build-essential python3 && \
+    apt clean && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /root/.local/lib/python3.7/site-packages /usr/local/lib/python3.7/dist-packages
+COPY . /src
+CMD ["python3", "/src/main.py"]
+EXPOSE 5000
--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 model = load_model("./mobilenetV2")
 
-origins = [
-    "http://localhost:3000",
-]
+origins = ["*"]
+methods = ["*"]
+headers = ["*"]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    CORSMiddleware, 
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = methods,
+    allow_headers = headers    
 )
 
 @app.get("/hello")
@@ -29,3 +29,6 @@ async def create_upload_file(file: UploadFile):
     res = predict(preprocess_img, model)
     res = res[0][:,1:]
     return {"result": res}
+
+if __name == "__main__":
+	run(app, host="0.0.0.0", port=5000)
