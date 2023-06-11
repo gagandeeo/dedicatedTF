fdfa7ff1fe64d3bca9288d3619d234863cb354c9
gagandeep-tiger <gagandeep.prasad@tigeranalytics.com>
2023-06-11 12:14:19 +0530

    adding req

--- test_model.py
+++ test_model.py
@@ -1,22 +1,22 @@
-# from tensorflow.keras.models import load_model
-# from tensorflow.keras.preprocessing import image
-# from tensorflow.keras import applications
-# from PIL import Image
-# import numpy as np
-# import io
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras import applications
+from PIL import Image
+import numpy as np
+import io
 
-# # Preprocessing image
-# def prepare_image(image_data):
-#     img_ = Image.open(io.BytesIO(image_data))
-#     if(img_.mode != 'RGB'):
-#         img_ = img_.convert('RGB')
-#     img_ = img_.resize((224,224))
-#     img_array = image.img_to_array(img_)
-#     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
-#     return applications.mobilenet.preprocess_input(img_array_expanded_dims)
+# Preprocessing image
+def prepare_image(image_data):
+    img_ = Image.open(io.BytesIO(image_data))
+    if(img_.mode != 'RGB'):
+        img_ = img_.convert('RGB')
+    img_ = img_.resize((224,224))
+    img_array = image.img_to_array(img_)
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return applications.mobilenet.preprocess_input(img_array_expanded_dims)
 
-# # Predict Image
-# def predict(img, model):
-#     predictions = np.array(model.predict(img))
-#     results = np.array(applications.imagenet_utils.decode_predictions(predictions))
-#     return results
+# Predict Image
+def predict(img, model):
+    predictions = np.array(model.predict(img))
+    results = np.array(applications.imagenet_utils.decode_predictions(predictions))
+    return results
