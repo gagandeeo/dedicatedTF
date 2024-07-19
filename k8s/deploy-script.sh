20d5dc95871d785dd7820a711dabcc46b672af61
gagandeeo <gagandeepprasad9@gmail.com>
2024-07-19 13:23:50 +0530

    fixing port character length

--- k8s/deploy-script.sh
+++ k8s/deploy-script.sh
@@ -3,3 +3,21 @@ gcloud container clusters create fruicy \
 --machine-type e2-medium \
 --num-nodes 2 \
 --disk-size 50
+
+# Get all informations
+#!/bin/bash
+
+# Common command
+common_command="kubectl get"
+
+# List of arguments for the common command
+args_list=(
+    "pods"
+    "deployments"
+    "services"
+)
+
+# Loop through the list and execute the common command with each set of arguments
+for args in "${args_list[@]}"; do
+    $common_command $args
+done
